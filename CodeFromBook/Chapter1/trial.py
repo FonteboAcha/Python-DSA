73a00fb367b0cf8b9a2bae336c35a292a4332c9b
FonteboAchah <fonteboachah@gmail.com>
2025-02-11 14:37:07 +0100

    learnt about conditionals and added to text doc

---
+++ CodeFromBook/Chapter1/trial.py
@@ -0,0 +1,8 @@
+# alpha = [1, 2, 3]
+# beta = alpha          # an alias for alpha
+# beta += [4, 5]        # extends the original list with two more elements
+# print(alpha)
+# beta = beta + [6, 7]  # reassigns beta to a new list [1, 2, 3, 4, 5, 6, 7]
+# print(beta )
+
+
